e91fb4acbbe83f4d290d9806a6dc0898b7876ed4
Admin <Admin@WIN-HF4C9I7C0E1>
2023-03-14 11:38:03 +0300

    Добавьте файлы проекта.

---
+++ ufuf/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ufuf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            TextBox btn = new TextBox();
+            btn.Parent = this;
+            btn.Location = new Point(e.X, e.Y);
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Button btn = new Button();
+            btn.Location = new Point(e.X, e.Y);
+            panel1.Controls.Add(btn);
+        }
+    }
+}
